2d0cbe8a3425ffb4e93657d8126dfe19c5c8a2b8
deveshbis <deveshbiswas.cse@gmail.com>
2024-04-08 14:46:57 +0600

    added banner section with four slides

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,32 +1,35 @@
-
+import bannerImg1 from '../assets/banner1.webp'
+import bannerImg2 from '../assets/banner2.png'
+import bannerImg3 from '../assets/banner3.jpg'
+import bannerImg4 from '../assets/banner4.png'
 
 const Banner = () => {
     return (
         <div >
             <div className="carousel w-full rounded-2xl">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+                    <img src={bannerImg1} className="w-full h-auto max-h-[500px]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide4" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+                    <img src={bannerImg2} className="w-full h-auto max-h-[500px]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                    <img src={bannerImg3} className="w-full h-auto max-h-[500px]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide4" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
-                    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+                    <img src={bannerImg4} className="w-full h-auto max-h-[500px]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide1" className="btn btn-circle">❯</a>
