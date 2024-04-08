9c6999dc96156ee2c2c278ab765837854c3a26ae
deveshbis <deveshbiswas.cse@gmail.com>
2024-04-08 16:11:18 +0600

    created footer section

--- src/Root/Roots.jsx
+++ src/Root/Roots.jsx
@@ -1,11 +1,14 @@
 import { Outlet  } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const Roots = () => {
     return (
         <div className="lg:px-32 md:px-12 px-5">
             <NavBar></NavBar>
+            
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
